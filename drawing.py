c1b591bee1214f799246ec5723f2ceb857e05667
M Rubern Chakravarthi <m.rubern.c@gmail.com>
2017-03-08 02:29:23 +0800

    abs

--- drawing.py
+++ drawing.py
@@ -40,7 +40,7 @@ for i in range(0,25):
     radius = np.random.randint(5, high=200)
     color = np.random.randint(0, high=256, size=(3,)).tolist()
 
-    pt = np.random.randint(0. high=300, size=(2,))
+    pt = np.random.randint(0, high=300, size=(2,))
 
     cv2.circle(canvas, tuple(pt), radius, color, -1)
 
