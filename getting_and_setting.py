4848433bf4a4dff10e71389733b307e2e7dd1362
M Rubern Chakravarthi <m.rubern.c@gmail.com>
2017-03-07 08:20:18 +0800

    get/set

---
+++ getting_and_setting.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+
+(b, g, r) = image[0, 0]
+print("Pixel at 0,0 - Red: {}, Green: {}, Blue: {}".format(r,g,b))
+
+image[0, 0] = (0, 0, 255)
+
+(b, g, r) = image[0, 0]
+print("Pixel at (0,0) - Red: {}: Green: {}, Blue: {}". format(r,g,b))
