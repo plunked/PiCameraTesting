df0c2ce5c24286672abb595e8ad327eeb6987ced
M Rubern Chakravarthi <m.rubern.c@gmail.com>
2017-03-08 02:09:23 +0800

    5.3

--- drawing.py
+++ drawing.py
@@ -11,3 +11,16 @@ red = (0,0,255)
 cv2.line(canvas, (300,0), (0,300), red, 3)
 cv2.imshow("Canvas", canvas)
 cv2.waitKey(0)
+
+cv2.rectangle(canvas, (10,10), (60,60), green)
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
+
+cv2.rectangle(canvas, (50,200), (200,225), red, 5)
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
+
+blue = (255,0,0)
+cv2.rectangle(canvas, (200,50), (225,125), blue, -1)
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
