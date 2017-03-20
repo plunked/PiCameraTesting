2a80a2de2d4420349d6570d34db4e79dbbff7bc0
M Rubern Chakravarthi <m.rubern.c@gmail.com>
2017-03-20 21:28:40 +0800

    face detection

---
+++ detectFaces.py
@@ -0,0 +1,21 @@
+from facedetector import FaceDetector
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--face", required = True, help = "path to the face cascade")
+ap.add_argument("-i", "--image", required = True, help = "path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BAYER_BG2GRAY)
+
+fd = FaceDetector(args["face"])
+faceRects = fd.detect(gray, scaleFactor = 1.1, minNeighbors = 5, minSize = (30, 30))
+print("I found {} faces(s)".format(len(faceRects)))
+
+for(x, y, w, h) in faceRects:
+    cv2.rectangle(image, (x, y), (x+ w, y+ h), (0, 255, 0), 2)
+
+cv2.imshow("Faces", image)
+cv2.waitKey(0)
---
+++ facedetector.py
@@ -0,0 +1,19 @@
+import cv2
+
+class FaceDetector:
+    def __init__(self, faceCascadePath):
+        self.faceCascade = cv2.CascadeClassifier(faceCascadePath)
+
+    def detect(self, image, scaleFactor = 1.1, minNeighbors = 5, minSize = (30, 30)):
+        rects = self.faceCascade.detectMultiScale(image, scaleFactor = scaleFactor, 
+        minNeighbors = minNeighbors, 
+        minSize = minSize, 
+        flags = cv2.CASCADE_SCALE_IMAGE)
+        return rects
+        
+    #scaleFactor refers to how much the image size is reduced at each image scale.
+    # Used to create the scale pyramid in order to detect faces at multiple scales in the image. 
+    # 
+    # minNeighbors is the number of neighbors each window should have in order to be considered a face.
+    # 
+    # minSize dictates the minimum size of the window.      
