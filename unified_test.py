bd8e9e7267b7fa5d172fbba149191f34865a425a
M Rubern Chakravarthi <m.rubern.c@gmail.com>
2017-03-25 12:31:01 +0800

    added testing program

---
+++ unified_test.py
@@ -0,0 +1,25 @@
+from camStream import camStream
+import argparse
+import time
+import imutils
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--picam", type=int, default=-1, help="wheter or not the Raspbery Pi camera should be used")
+args = vars(ap.parse_args())
+
+videoStream = camStream(usePiCam=args["picam"] > 0).start()
+time.sleep(2.0)
+
+while True:
+    frame = videoStream.read()
+    frame = imutils.resize(frame, width=400)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+videoStream.stop()
