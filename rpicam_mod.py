1f669909b030b65064ca9a433f842c296ddba802
M Rubern Chakravarthi <m.rubern.c@gmail.com>
2017-03-25 12:19:12 +0800

    minor

--- rpicam_mod.py
+++ rpicam_mod.py
@@ -18,6 +18,7 @@ time.sleep(2.0)
 
 #constructing the detector
 fd = FaceDetector(args["face"])
+print("test0")
 
 
 for frames in camera.stream():
@@ -29,6 +30,7 @@ for frames in camera.stream():
     
     faceRects=fd.detect(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
     frameClone = frame.copy()
+    print("test1")
 
     for (fX, fY, fW, fH) in faceRects:
         cv2.rectangle(frameClone, (fX, fY), (fX+fW, fY+fH), (0, 255, 0), 2)
@@ -36,7 +38,7 @@ for frames in camera.stream():
 	# show our detected faces, then clear the frame in
 	# preparation for the next frame
     cv2.imshow("Face", frameClone)
-    rawCapture.truncate(0)
+    camera.rawCapture.truncate(0)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
