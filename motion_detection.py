cc5b3b83eb4c62b5cfbbf573bbb529009556dce3
M Rubern Chakravarthi <m.rubern.c@gmail.com>
2017-03-28 22:57:04 +0800

    trialing md

--- motion_detection.py
+++ motion_detection.py
@@ -24,24 +24,23 @@ else:
 
 firstFrame = None
 
-for frames in camera.capture_continuous(rawCapture, format="bgr", use_video_port = True):
+for frames in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 
     frame = frames.array
     text = "Unoccupied"
-    
     frame = imutils.resize(frame, width=300)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0 )
 
     if firstFrame is None:
-        firstFrame = gray
+        firstFrame = gray.copy().astype("float")
+        rawCapture.truncate(0)
         continue
-
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
-
+    cv2.accumulateWeighted(gray, firstFrame, 0.5)      
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(firstFrame))
+    thresh = cv2.threshold(frameDelta, 2, 255, cv2.THRESH_BINARY)[1]
     thresh = cv2.dilate(thresh, None, iterations=2)
-    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    (_, cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for c in cnts:
         if cv2.contourArea(c) < args["min area"]:
@@ -50,10 +49,7 @@ for frames in camera.capture_continuous(rawCapture, format="bgr", use_video_port
         (x, y, w, h) = cv2.boundingRect(c)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         text = "Occupied"
-        cv2.putText(frame, "Room Status: {}".format(text), (10, 20), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I: %M:%S%p"), (10, frame.shape[0] - [10]), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-
-        cv2.imshow("Security Feed", frame)
+        cv2.imshow("Feed", frame)
         cv2.imshow("Thresh", thresh)
         cv2.imshow("Frame Delta", frameDelta)
         key = cv2.waitKey(1) & 0xFF
